b132ff6d3245d2b4ea20c857f69130145548bba4
Danilo Siqueira <danilosic95@gmail.com>
2020-05-18 05:55:03 -0300

    upload-map-tile

--- src/components/map-load.js
+++ src/components/map-load.js
@@ -3,19 +3,22 @@
 import React, { createRef, Component} from 'react'
 import { Map, ImageOverlay } from 'react-leaflet'
 
+import ImageTile from './image-tile'
+
 export const pointerIcon = new L.Icon({
-    iconUrl: '../assets/pointerIcon.svg',
-    iconRetinaUrl: '../assets/pointerIcon.svg',
-    iconAnchor: [5, 55],
-    popupAnchor: [10, -44],
+    iconUrl: '../assets/interface.svg',
+    iconRetinaUrl: '../assets/interface.svg',
+    //iconAnchor: [5, 55],
+    //popupAnchor: [10, -44],
     iconSize: [25, 55],
-    shadowUrl: '../assets/marker-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [20, 92],
+    //shadowUrl: '../assets/marker-shadow.png',
+    //shadowSize: [68, 95],
+    //shadowAnchor: [20, 92],
   })
   
-type State = {
+type State = { 
     hasLocation: boolean,
+    imageUrl: String,
     latlng: {
       lat: number,
       lng: number,
@@ -25,6 +28,7 @@ type State = {
 export default class MapLoad extends Component<{}, State> {
     state = {
       hasLocation: false,
+      imageUrl:"./assets/rpgcast_01.jpg",
       latlng: {
         lat: 51.505,
         lng: -0.09,
@@ -38,7 +42,6 @@ export default class MapLoad extends Component<{}, State> {
     i_polygons = 0;
   
     mapRef = createRef<Map>()
-  
     // handleClick = () => {
     //   const map = this.mapRef.current
     //   if (map != null) {
@@ -49,55 +52,74 @@ export default class MapLoad extends Component<{}, State> {
     
 
     onMapClick = (e) => {
-       const map = this.mapRef.current
-        if (!e.originalEvent.defaultPrevented) {
-            //console.log(e.latlng);
-            this.points[this.i_points] = e.latlng;
-            this.layers_mark[this.i_points] = L.marker(this.points[this.i_points], {icon: pointerIcon}).addTo(map.leafletElement);
-            this.layers_mark[this.i_points].on('click', this.click_mark, ['a']);
-            this.i_points++;
-        }
+      const map = this.mapRef.current
+      if (!e.originalEvent.defaultPrevented) {
+          console.log(e.latlng);
+          this.points[this.i_points] = e.latlng;
+          this.layers_mark[this.i_points] = L.marker(this.points[this.i_points], {icon: pointerIcon}).addTo(map.leafletElement);
+          this.layers_mark[this.i_points].on('click', this.click_mark, ['a']);
+          this.i_points++;
+      }
+    }
+
+
+    onRegionClick = (e) => {
+      e.originalEvent.preventDefault();
     }
 
     click_mark = (evt) => {
-
-        this.layers_polygon[this.i_polygons] = L.polygon(this.points, {
-            color: 'red'
-        }).addTo(this.mapRef.current.leafletElement);
-        for (var i = this.i_points - 1; i >= 0; i--) {
-            this.layers_mark[i].remove();
-        }
-        this.points = [];
-        this.i_points = 0;
-        this.i_polygons++;
+      console.log(this.points)
+      this.layers_polygon[this.i_polygons] = L.polygon(this.points, {
+          color: 'red'
+      }).addTo(this.mapRef.current.leafletElement).on('click', this.onRegionClick);
+      for (var i = this.i_points - 1; i >= 0; i--) {
+          this.layers_mark[i].remove();
+      }
+      this.points = [];
+      this.i_points = 0;
+      this.i_polygons++;
 
     }
-  
+
+    getBounds = () => {
+
+      return [[0,0], [85,300]];
+    }
+
+    onChangeHandler=event=>{
+      this.setState({
+        hasLocation:true,
+        imageUrl: event.target.files[0]
+      })
+    }
+
     render() {
-      const marker = this.state.hasLocation ? (
-        <Marker position={this.state.latlng}>
-          <Popup>You are here</Popup>
-        </Marker>
-      ) : null
-  
-      return (
-        <Map 
-          id="mapid" 
-          center={[0, 0]} 
-          zoom={3} 
-          maxBounds={new L.LatLngBounds([0.7,1.4], [85,300])}
-          minZoom={2}
-          maxZoom={5}
-          ref={this.mapRef}
-          onClick={this.onMapClick}
-      
-          >
-          <ImageOverlay 
-            url="./assets/map-test-tile.jpg" 
-            bounds={[[0.7,1.4], [85,300]]}
-            
-          />
-        </Map>
-      )
+      //console.log(this.state.imageUrl)
+      if(this.state.hasLocation){
+        return (
+          <Map 
+            id="mapid" 
+            center={[0, 0]} 
+            zoom={1} 
+            maxBounds={new L.LatLngBounds([0,0], [100,300])}
+            //minZoom={2}
+            //maxZoom={7}
+            ref={this.mapRef}
+            onClick={this.onMapClick}
+        
+            >
+            <ImageTile 
+              src= {this.state.imageUrl}
+            />
+          </Map>
+        )
+      }
+      else{
+        return(
+          <div style={{textAlign: 'center', lineHeight: '50'}}>
+            <input type="file" name="file" onChange={this.onChangeHandler}/>
+          </div>
+        )
+      }
     }
   }
---
+++ src/components/message.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Message extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( <div></div> );
+    }
+}
+ 
+export default Message;
