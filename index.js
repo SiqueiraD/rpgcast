7970d08c2efb95fe0f7f82372702045c7a9dfc97
Danilo Siqueira <danilo_siqueira@id.uff.br>
2021-12-13 05:33:55 -0300

    teste heroku postgres

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 express()
     //.use(express.static(path.join(__dirname, 'public'))) arquivos estáticos 
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.render('index'))
+    .get('/db', async (req, res) => {
+        try {
+          const client = await pool.connect();
+          const result = await client.query('SELECT * FROM test_table');
+          const results = { 'results': (result) ? result.rows : null};
+          res.render('pages/db', results );
+          client.release();
+        } catch (err) {
+          console.error(err);
+          res.send("Error " + err);
+        }
+      })
+      .post('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query(req.body.query);
+            const results = { 'results': (result) ? result.rows : null};
+            res.render('pages/db', results );
+            client.release();
+          } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+          }
+      })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`))
