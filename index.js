a250f52d17dc8b05fa279c362df050ca64e8526b
Danilo Siqueira <danilo_siqueira@id.uff.br>
2021-12-13 05:39:15 -0300

    falha no parametro results (nao existe no catch)

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ express()
         } catch (err) {
           console.error(err);
           //res.send("Error " + err);
-          res.render('pages/db', results );
+          res.render('pages/db' );
         }
       })
       .post('/db', async (req, res) => {
