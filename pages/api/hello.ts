93b10fb3fac7e24cfd6dc7d6a517cdc8d32b38d3
Danilo Siqueira <danilo_siqueira@id.uff.br>
2023-02-25 13:58:52 -0300

    ajuste para teste b4app online

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios, { AxiosResponse } from 'axios'
 
 type Data = {
-  name: string
+  statusCode: number
+  body: any
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const response = await axios
+      .request({
+        url: `https://api.agify.io?name=michael`,
+        method: 'get',
+      })
+      .then((resp: AxiosResponse) => ({
+        statusCode: resp.status,
+        body: resp.data,
+      }))
+      .catch((error: any) => ({
+        statusCode: error.response.status,
+        body: error.response.data,
+      }))
+
+  res.status(200).json(response)
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -20,6 +20,10 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
+          <p>
+            Entre em&nbsp;
+            <code className={styles.code}>userLogin ou api/hello</code>
+          </p>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
