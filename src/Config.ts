e385681c89549a10d4940d11ba17915ceafb60d7
Danilo Siqueira <danilo_siqueira@id.uff.br>
2023-02-25 01:26:56 -0300

    reset projeto

--- src/Config.ts
+++
@@ -1,6 +0,0 @@
-
-export default {
-    PARSE_APPLICATION_ID: "g1Je53l3sHXp1LLauACcBvKGlDTJjztd4rKefQ1T",
-    PARSE_HOST_URL: "https://parseapi.back4app.com/",
-    PARSE_JAVASCRIPT_KEY: "JmFvBpEom4rRwV3jKvS0LcnLbADzM4jqG24PdRce"
-};
